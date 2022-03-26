b0ca0b597c2d4bf1c6290f857730887f50fa01e7
Erlangga <erlangga.sukmana02@gmail.com>
2022-03-26 20:45:30 +0700

    Homework3mod2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { HomeComponent } from "./Components/home/index";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomeComponent />
     </div>
   );
 }
-
-export default App;
---
+++ src/Components/home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AlbumList from "../track/index";
+
+function HomeComponent() {
+  return (
+    <>
+      <div className="Container">
+        <AlbumList />
+      </div>
+    </>
+  );
+}
+
+export { HomeComponent };
+
